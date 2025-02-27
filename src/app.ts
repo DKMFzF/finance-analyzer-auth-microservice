fc8326e675b54383531846dc0afb68c62af36d1a
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-27 09:24:59 +0700

    add security

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,23 @@ import { LOGS } from './utils/custom.logs';
 import logger from './utils/logger';
 import { errorHandler } from './middleware/error.middleware';
 
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+
+
 const app = express();
 app.use(express.json());
 
 app.use('/', routes);
 app.use(errorHandler);
+
+app.use(helmet());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+});
+app.use(limiter);
 
 logger.info(LOGS.SERVICE_INIT);
 
