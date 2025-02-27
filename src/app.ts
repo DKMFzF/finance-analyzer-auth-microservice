56db12eeaee078ad9e5d42058b76e466f721f4e4
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-27 11:04:54 +0700

    add compose data

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,21 @@ import logger from './utils/logger';
 import { errorHandler } from './middleware/error.middleware';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
+// @ts-ignore
+import compression from 'compression';
 
-const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
 const app = express();
 app.use(express.json());
 
 app.use('/', routes);
 app.use(errorHandler);
 
+app.use(compression());
 app.use(helmet());
-app.use(limiter);
+app.use(rateLimit({ 
+  windowMs: 15 * 60 * 1000, 
+  max: 100 
+}));
 
 logger.info(LOGS.SERVICE_INIT);
 
