141b700b4d1b3ac17f446d9f4e88221aeb4ddb00
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-27 08:34:42 +0700

    add module validation data

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,22 +1,28 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import axios from 'axios';
 // import { SECRET_KEY, USER_SERVICE_URL } from '../utils/jwt';
 import { config } from "../config/index.config";
 // @ts-ignore
 import jwt from 'jsonwebtoken';
 import { LoginRequestBody, User } from '../types/user';
+import { loginSchema } from '../utils/validation';
+import { checkUserCredentials } from "../services/auth.service";
 
-export const login = async (req: Request<{}, {}, LoginRequestBody>, res: Response) => {
-    const { login, password } = req.body;
+/**
+ * модуль запроса для входа в систему 
+ */
 
+export const login = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const response = await axios.post<User>(`${config.USER_SERVICE_URL}/check-user`, { login, password });
-        const user = response.data;
+        const { error } = loginSchema.validate(req.body);
+        if (error) return res.status(400).json({ message: error.details[0].message });
+
+        const { login, password } = req.body;
+        const user = await checkUserCredentials(login, password);
 
         const token = jwt.sign({ userId: user.id, login: user.login }, config.SECRET_KEY, { expiresIn: '1h' });
         res.json({ token });
     } catch (error) {
-        res.status(401).json({ message: 'Неверные учетные данные' });
-        console.log("[ERROR]: Invalid credentials");
+        next(error);
     }
 };
